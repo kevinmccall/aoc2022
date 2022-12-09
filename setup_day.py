e395daf6fb21875905c34bdc065ff3d652773563
ErakiJeff <erakiproj99@gmail.com>
2022-12-09 11:50:35 -0500

    created a script to setup the days for me

---
+++ setup_day.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import sys
+import argparse
+import subprocess
+import pathlib
+from secret import SESSION
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Read input")
+    parser.add_argument("day", type=int)
+    parser.add_argument("name", type=str)
+    parser.add_argument("--year", type=int, default=2022)
+    args = parser.parse_args()
+
+    filename = "_".join(args.name.lower().split()) + ".py"
+    dest = pathlib.Path(f"./{args.day}/")
+    cmd = 'curl https://adventofcode.com/{}/day/{}/input --cookie "session={}"'.format(
+        args.year, args.day, SESSION
+    )
+    output = subprocess.check_output(cmd, shell=True)
+    with (dest / "input.txt").open("w", encoding="utf8") as writer:
+        print(output.decode("utf-8"), end="", file=writer)
+    file = dest / filename
+    if not file.exists():
+        file.touch()
+        with file.open("w", encoding="utf8") as writer:
+            writer.write(
+                """data = None
+p1 = 0
+p2 = 0
+with open("input.txt", "r", encoding="utf8") as reader:
+    data = [x.strip() for x in reader.readlines()]
+
+"""
+            )
