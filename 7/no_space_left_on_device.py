d268101631c1342d1370abeb69295c2086b112f9
kevinmccall <erakiproj99@gmail.com>
2022-12-10 20:38:10 -0500

    made objectively good changes (i think)

--- 7/no_space_left_on_device.py
+++ 7/no_space_left_on_device.py
@@ -1,47 +1,54 @@
 data = None
 p1 = 0
 p2 = 0
-with open("7/input.txt", "r", encoding="utf8") as reader:
+with open("input.txt", "r", encoding="utf8") as reader:
     data = [x.strip() for x in reader.readlines()]
 
-size_under_100_000 = 0
+
+P1_TARGET = 100_000
+P2_TOTAL_SIZE = 70_000_000
+P2_SIZE_NEEDED = 30_000_000
+
+size_under_target = 0
 
 
 class Node:
     def __init__(self, name) -> None:
-        self.children = {}
-        self.file_size = 0
+        self.dirs = {}
+        self.files = []
         self.name = name
         self.parent = None
-        self.is_dir = False
-        self.total_size = None
+        self.cached_size = None
 
-    def add_child(self, name, size, is_dir):
+    def add_file(self, size):
+        self.files.append(size)
+
+    def add_dir(self, name):
         child = Node(name)
-        child.file_size = size
         child.parent = self
-        child.is_dir = is_dir
-        self.children[name] = child
+        self.dirs[name] = child
 
-    def get_child(self, name):
-        return self.children[name]
+    def get_dir(self, dirname):
+        return self.dirs[dirname]
 
     def get_parent(self):
         return self.parent
 
     def get_size(self):
-        if self.total_size is None:
-            size = self.file_size
-            for child in self.children.values():
+        if self.cached_size is None:
+            size = 0
+            for child in self.dirs.values():
                 size += child.get_size()
+            for filesize in self.files:
+                size += filesize
 
-            if size <= 100_000 and self.is_dir:
-                global size_under_100_000
-                size_under_100_000 += size
-            self.total_size = size
+            if size <= P1_TARGET:
+                global size_under_target
+                size_under_target += size
+            self.cached_size = size
             return size
         else:
-            return self.total_size
+            return self.cached_size
 
 
 current_dir: Node = None
@@ -56,34 +63,13 @@ for line in data:
             else:
                 if current_dir is None:
                     current_dir = Node(split[2])
-                    current_dir.is_dir = True
                     head = current_dir
                 else:
-                    current_dir = current_dir.get_child(split[2])
+                    current_dir = current_dir.get_dir(split[2])
     elif split[0] == "dir":
-        current_dir.add_child(split[1], 0, True)
+        current_dir.add_dir(split[1])
     elif split[0].isnumeric():
-        current_dir.add_child(split[1], int(split[0]), False)
+        current_dir.add_file(int(split[0]))
 
-head.get_size()
-
-
-print(size_under_100_000)
-
-all_n = []
-
-
-def all_nodes(node):
-    required_size = min(30000000, 70000000 - head.total_size)
-    if not node.is_dir or node.total_size < required_size:
-        return
-    for child in node.children.values():
-        all_nodes(child)
-    all_n.append(node)
-
-
-all_nodes(head)
-all_n.sort(key=lambda x: x.total_size)
-print(all_n[0].total_size)
-
-# 6483416 too high
+print(head.get_size())
+print(size_under_target)
